f902a4e8517833dc1fc5e2f359dd6fff0d5bd2db
Alejandro Goicochea <panflete@Alejandros-Mac-mini.local>
2023-11-08 21:22:44 -0500

    feat: add github get commits service function

--- backend/src/github/github.service.ts
+++ backend/src/github/github.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Observable, catchError, map } from 'rxjs';
+import { GitHubCommit } from './github.interface';
 
 @Injectable()
-export class GithubService {}
+export class GithubService {
+  constructor(private httpService: HttpService) {}
+
+  getCommits(
+    username: string,
+    repo: string,
+    branch: string,
+    limit: number,
+    page: number,
+  ): Observable<GitHubCommit[]> {
+    return this.httpService
+      .get<GitHubCommit[]>(
+        `https://api.github.com/repos/${username}/${repo}/commits`,
+        {
+          params: {
+            sha: branch,
+            per_page: limit,
+            page,
+          },
+        },
+      )
+      .pipe(
+        map((response) => response.data),
+        catchError((error) => {
+          if (error.response?.status === HttpStatus.NOT_FOUND) {
+            throw new HttpException(
+              'Repository not found',
+              HttpStatus.NOT_FOUND,
+            );
+          } else {
+            throw new HttpException(
+              'Internal server error',
+              HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+          }
+        }),
+      );
+  }
+}
