8ff857aa8ffd932317e1dd4a408acb206d296017
Alejandro Goicochea <panflete@Alejandros-Mac-mini.local>
2023-11-08 21:24:11 -0500

    feat: add get commits controller route

--- backend/src/github/github.controller.ts
+++ backend/src/github/github.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { GithubService } from './github.service';
 
 @Controller({ path: 'github/repos' })
 export class GithubController {
   constructor(private readonly githubService: GithubService) {}
+
+  @Get(':username/:repo/commits')
+  getCommits(
+    @Param('username') username: string,
+    @Param('repo') repo: string,
+    @Query('branch') branch?: string,
+    @Query('limit') limit?: number,
+    @Query('page') page?: number,
+  ) {
+    return this.githubService.getCommits(username, repo, branch, limit, page);
+  }
 }
--- backend/src/github/github.module.ts
+++ backend/src/github/github.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { GithubService } from './github.service';
+import { HttpModule } from '@nestjs/axios';
+import { GithubController } from './github.controller';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    HttpModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        headers: {
+          Authorization: `Bearer ${configService.get('GITHUB_TOKEN')}`,
+          'X-Github-Api-Version': '2022-11-28',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [GithubController],
   providers: [GithubService],
 })
 export class GithubModule {}
