3aa47351e74c64c355dd67e0fcc6b3d18b35f661
Alejandro Goicochea <alejandrogoicochea@Alejandros-MacBook-Pro.local>
2023-11-12 17:42:36 -0500

    feat: add fetch branches service

--- frontend/src/services/githubService.tsx
+++ frontend/src/services/githubService.tsx
@@ -1,4 +1,5 @@
-import { GitHubCommit, SearchParams } from '../types/commit';
+import { GitHubBranch } from "../types/branches";
+import { GitHubCommit, SearchParams } from "../types/commit";
 
 export const fetchCommits = async (params: SearchParams) => {
   const baseURL: string = import.meta.env.VITE_BACKEND_URL;
@@ -23,4 +24,24 @@ export const fetchCommits = async (params: SearchParams) => {
   console.log(commits);
 
   return commits;
+};
+
+export const fetchBranches = async (params: SearchParams) => {
+  const baseURL: string = import.meta.env.VITE_BACKEND_URL;
+
+  const { author, repo } = params;
+  if (!author || !repo) {
+    return [];
+  }
+  const response = await fetch(
+    `${baseURL}/github/repos/${author}/${repo}/branches`
+  );
+  if (!response.ok) {
+    return [];
+  }
+
+  const branches: GitHubBranch[] = await response.json();
+  console.log(branches);
+
+  return branches;
 };
