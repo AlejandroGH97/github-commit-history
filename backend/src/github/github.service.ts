bffb82b4c5ab47d80b38a9e265f4ebbe2be20b94
Alejandro Goicochea <alejandrogoicochea@Alejandros-MacBook-Pro.local>
2023-11-12 17:36:23 -0500

    feat: add repo branches service

--- backend/src/github/github.service.ts
+++ backend/src/github/github.service.ts
@@ -2,7 +2,7 @@ import { HttpService } from '@nestjs/axios';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Observable, catchError, map } from 'rxjs';
 
-import { GitHubCommit } from './github.interface';
+import { GitHubBranch, GitHubCommit } from './github.interface';
 
 @Injectable()
 export class GithubService {
@@ -23,6 +23,27 @@ export class GithubService {
           page,
         },
       })
+      .pipe(
+        map((response) => response.data),
+        catchError((error) => {
+          if (error.response?.status === HttpStatus.NOT_FOUND) {
+            throw new HttpException(
+              'Repository not found',
+              HttpStatus.NOT_FOUND,
+            );
+          } else {
+            throw new HttpException(
+              'Internal server error',
+              HttpStatus.INTERNAL_SERVER_ERROR,
+            );
+          }
+        }),
+      );
+  }
+
+  getBranches(username: string, repo: string): Observable<GitHubBranch[]> {
+    return this.httpService
+      .get<GitHubBranch[]>(`/repos/${username}/${repo}/branches`)
       .pipe(
         map((response) => response.data),
         catchError((error) => {
